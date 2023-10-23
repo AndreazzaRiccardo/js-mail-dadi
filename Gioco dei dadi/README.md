63626457a777796053dfadc99920177eba3d5323
AndreazzaRiccardo <riccardo.andreazza90@gmail.com>
2023-10-23 17:29:36 +0200

    add: exercise dice complete

---
+++ Gioco dei dadi/README.md
@@ -0,0 +1,14 @@
+### ESERCIZIO: 
+
+**Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.**
+
+### LOGICA:
+
+1. Creare due variabili una per il player e l'altra per il bot;
+2. Generare due numeri casuali per ciascuno;
+3. Determinare il vincitore in base a chi ottiene il numero più alto;
+4. Preparare il messaggio da visualizzare;
+
+### OUTPUT:
+Stampare il messaggio a schermo
---
+++ Gioco dei dadi/js/script.js
@@ -0,0 +1,50 @@
+let numberPlayer = Math.round(Math.random() * 5) + 1;
+let numberBot = Math.round(Math.random() * 5) + 1;;
+let message = "";
+
+if (numberPlayer < numberBot) {
+    message = "Mi dispiace, hai perso"
+} else if (numberPlayer > numberBot) {
+    message = "Complimenti, mi hai battuto"
+} else if (numberPlayer === numberBot) {
+    message = "Questa volta siamo pari, tiriamo ancora!!!"
+}
+
+document.querySelector("h1").innerHTML = message;
+const dicePlayer = document.getElementById("player");
+const diceBot = document.getElementById("bot");
+
+if(numberBot === 1) {
+    diceBot.classList.add("one")
+} else if (numberBot === 2) {
+    diceBot.classList.add("two")
+} else if (numberBot === 3) {
+    diceBot.classList.add("three")
+} else if (numberBot === 4) {
+    diceBot.classList.add("four")
+} else if (numberBot === 5) {
+    diceBot.classList.add("five")
+} else if (numberBot === 6) {
+    diceBot.classList.add("six")
+}
+
+if(numberPlayer === 1) {
+    dicePlayer.classList.add("one")
+} else if (numberPlayer === 2) {
+    dicePlayer.classList.add("two")
+} else if (numberPlayer === 3) {
+    dicePlayer.classList.add("three")
+} else if (numberPlayer === 4) {
+    dicePlayer.classList.add("four")
+} else if (numberPlayer === 5) {
+    dicePlayer.classList.add("five")
+} else if (numberPlayer === 6) {
+    dicePlayer.classList.add("six")
+}
+
+const play = document.getElementById("go");
+
+play.addEventListener("click", function(){
+    window.location.reload();
+})
+
